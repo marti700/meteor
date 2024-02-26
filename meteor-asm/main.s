ee5d6fbe1882a9316652f5286f6121dd0d92b96d
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2024-02-25 21:27:00 -0400

    Testing timers

--- meteor-asm/main.s
+++ meteor-asm/main.s
@@ -5,44 +5,71 @@
     .text
     .thumb_func
 
-    .equ onesecond, 16000000
+    .equ onesecond, 1000000
+    .equ TIMER0_BASE, 0x40008000
+    .equ GIPIO_BASE, 0x50000000
+    .equ TIMER_PRESCALER, 4
 
     main:
-        // set pin 2 as output
-        ldr R0, =0x50000518 // load the DIRSET register to R0
+        bl init_gpio
+        bl reset_timer
+        bl start_timer
+        bl blink_loop
+
+    init_gpio:
+        ldr R0, =GIPIO_BASE // load the DIRSET register to R0
         ldr R1, [R0] // load R0 contents to R1
         orr R1, #(1<<2) // set bit 2 of DIRSET to 1
-        str R1, [R0] // store the new value of R1 to R0, this will set pin 2 to ouput
-        b loop // branch to loop 
+        str R1, [R0, #0x518] // store the new value of R1 to R0, this will set pin 2 to ouput
+        bx lr
 
-    loop:
-        ldr R5, =onesecond // 64 million clock cycles is aproximately 1 second
-        bl turn_led_on // turn led on 
-        bl delay1second // wait one second
-        ldr R5, =onesecond // 64 million clock cycles is aproximately 1 second
-        bl turn_led_off // turn led off 
-        bl delay1second // wait one second
-        b loop // start over
-    
-    delay1second:
-        // substract 1 from r5, and store the new value in r5
-        // eventually r5 is going to be 
-        subs R5,R5, #1   
-        bne delay1second // if 
+    reset_timer:
+        ldr r0, =TIMER0_BASE
+        @ do and r1, #(1<<0) instead?
+        mov r1, #0
+        str r1, [r0, #0x004] // stop timer
+        str r1, [r0, #0x00C] // clear timer
+        str r1, [r0, #0x140] // clear events
+
+        ldr r1, =TIMER_PRESCALER
+        str r1, [r0, #0x510]
+
+        @ set caputure register to onesecond
+        ldr r1, =onesecond
+        str r1, [r0, #0x540]
+
+        @ set bit mode
+        mov r1, #3
+        str r1, [r0, #0x508]
+
         bx lr
     
-    turn_led_on:
-        ldr R0, =0x50000508 // load OUTSET register to R0
+    start_timer:
+        ldr r0, =TIMER0_BASE
+        @ do and r1, #(1<<0) instead?
+        mov r1, #1
+        str r1, [r0, #0x000]
+
+        bx lr
+    
+    blink_loop:
+        @ ldr r2, =0x40008140
+        @ ldr r3, =0x40008540
+        @ ldr r4, =0x50000504
+        ldr r1, [r0, #0x140]
+        tst r1, #1
+        beq blink_loop
+
+        bl toggle_led
+        bl reset_timer
+        bl start_timer
+        b blink_loop
+
+    toggle_led:
+        ldr R0, =GIPIO_BASE // load OUT register to R0
         ldr R1, [R0] // load contents of R0 to R1
-        orr R1, #(1<<2) // set bit 2 of OUTSET to 1
-        str R1, [R0] // store contents the new value of R1 to R0, this will set pin 2 high
+        eor R1, #(1<<2) // toogle bit 2 of OUT register on/off
+        str R1, [R0, #0x504] // store contents the new value of R1 to R0, this will set pin 2 high
         bx lr
 
-    turn_led_off:
-        ldr R0, =0x5000050C // load OUTCLR register to R0
-        ldr R1, [R0] // load contents of R0 to R1
-        orr R1, #(1<<2) // set bit 2 of OUTCLR to 1
-        str R1, [R0] // store contents the new value of R1 to R0, this will set pin 2 low
-        bx lr
-
-
+ 
