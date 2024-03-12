7152e9c435a3b84a4fd0cfea10fcf86a629681a2
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2024-03-12 11:52:48 -0400

    Make a led blink using thhe nRF52883 timer

--- meteor-asm/main.s
+++ meteor-asm/main.s
@@ -25,11 +25,17 @@
 
     reset_timer:
         ldr r0, =TIMER0_BASE
-        @ do and r1, #(1<<0) instead?
-        mov r1, #0
+
+        mov r1, #1
         str r1, [r0, #0x004] // stop timer
         str r1, [r0, #0x00C] // clear timer
+
+        mov r1, #0 
         str r1, [r0, #0x140] // clear events
+
+        @ set bit mode to 32 bits
+        mov r1, #3
+        str r1, [r0, #0x508]
 
         ldr r1, =TIMER_PRESCALER
         str r1, [r0, #0x510]
@@ -38,24 +44,16 @@
         ldr r1, =onesecond
         str r1, [r0, #0x540]
 
-        @ set bit mode
-        mov r1, #3
-        str r1, [r0, #0x508]
-
         bx lr
     
     start_timer:
         ldr r0, =TIMER0_BASE
-        @ do and r1, #(1<<0) instead?
         mov r1, #1
         str r1, [r0, #0x000]
 
         bx lr
     
     blink_loop:
-        @ ldr r2, =0x40008140
-        @ ldr r3, =0x40008540
-        @ ldr r4, =0x50000504
         ldr r1, [r0, #0x140]
         tst r1, #1
         beq blink_loop
@@ -67,7 +65,7 @@
 
     toggle_led:
         ldr R0, =GIPIO_BASE // load OUT register to R0
-        ldr R1, [R0] // load contents of R0 to R1
+        ldr R1, [R0, #0x504] // load contents of R0 to R1
         eor R1, #(1<<2) // toogle bit 2 of OUT register on/off
         str R1, [R0, #0x504] // store contents the new value of R1 to R0, this will set pin 2 high
         bx lr
