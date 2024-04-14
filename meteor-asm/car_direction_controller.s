5805f35fa886aaff21e7c102959614bcca5a84ff
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2024-04-14 18:05:56 -0400

    created car directions controller

---
+++ meteor-asm/car_direction_controller.s
@@ -0,0 +1,116 @@
+  .thumb
+  .syntax unified
+
+  .global go_forward
+  .global car_stop
+  .global go_backwards
+  .global rotate_left
+  .global rotate_right
+  .global go_sideways_right
+  .global go_sideways_left
+  .global go_digonal_upwards_right
+  .global go_diagonal_downwards_left
+  .global go_diagonal_upwards_left
+  .global go_diagonal_downwards_right
+
+  .text
+  .thumb_func
+
+  go_forward:
+    push {lr}
+    bl m1_forward
+    bl m2_forward
+    bl m3_forward
+    bl m4_forward
+    pop {lr}
+    bx lr
+
+  car_stop:
+    push {lr}
+    bl m1_stop
+    bl m2_stop
+    bl m3_stop
+    bl m4_stop
+    pop {lr}
+    bx lr
+
+  go_backwards:
+    push {lr}
+    bl m1_reverse
+    bl m2_reverse
+    bl m3_reverse
+    bl m4_reverse
+    pop {lr}
+    bx lr
+
+  rotate_left:
+    push {lr}
+    bl m1_forward
+    bl m2_forward
+    bl m3_reverse
+    bl m4_reverse
+    pop {lr}
+    bx lr
+
+  rotate_right:
+    push {lr}
+    bl m1_reverse
+    bl m2_reverse
+    bl m3_forward
+    bl m4_forward
+    pop {lr}
+    bx lr
+
+  go_sideways_right:
+    push {lr}
+    bl m1_forward
+    bl m2_reverse
+    bl m3_forward
+    bl m4_reverse
+    pop {lr}
+    bx lr
+
+go_sideways_left:
+    push {lr}
+    bl m1_reverse
+    bl m2_forward
+    bl m3_reverse
+    bl m4_forward
+    pop {lr}
+    bx lr
+
+go_digonal_upwards_right:
+    push {lr}
+    bl m1_forward
+    bl m2_stop
+    bl m3_forward
+    bl m4_stop
+    pop {lr}
+    bx lr
+
+go_diagonal_downwards_left:
+    push {lr}
+    bl m1_reverse
+    bl m2_stop
+    bl m3_reverse
+    bl m4_stop
+    pop {lr}
+    bx lr
+
+go_diagonal_upwards_left:
+    push {lr}
+    bl m1_stop
+    bl m2_forward
+    bl m3_stop
+    bl m4_forward
+    pop {lr}
+    bx lr
+
+go_diagonal_downwards_right:
+    push {lr}
+    bl m1_stop
+    bl m2_reverse
+    bl m3_stop
+    bl m4_reverse
+    pop {lr}
+    bx lr
--- meteor-asm/main.s
+++ meteor-asm/main.s
@@ -10,10 +10,7 @@
   main:
     @ bl measure_distance
     bl init_PCA9685
-    bl m1_forward
-    bl m2_forward
-    bl m3_forward
-    bl m4_forward
+    bl go_forward
     ldr r1, =one_second
 
   wait_one_second:
@@ -21,10 +18,7 @@
     cmp r1, #0
     bne wait_one_second
 
-    bl m1_stop
-    bl m2_stop
-    bl m3_stop
-    bl m4_stop
+    bl car_stop
     ldr r1, =one_second
 
   wait_oone_second:
@@ -32,10 +26,7 @@
     cmp r1, #0
     bne wait_oone_second
 
-    bl m1_reverse
-    bl m2_reverse
-    bl m3_reverse
-    bl m4_reverse
+    bl go_backwards
     ldr r1, =one_second
 
   wait_ooone_second:
